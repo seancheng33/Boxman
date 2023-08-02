d64447b7d6b174c9056c453a1a3b52e3cf859ada
seancheng33 <aya234@163.com>
2023-08-03 02:42:57 +0800

    主菜单界面添加了背景图

--- boxman/Boxman.service.worker.js
+++ boxman/Boxman.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1690204358|20428909855";
+const CACHE_VERSION = "1691001623|1127255157";
 const CACHE_PREFIX = "Boxman-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "Boxman.offline.html";
